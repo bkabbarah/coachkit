a36d58c4dbe7780861545052f4a849e22e557bc0
bkabbarah <basharkabbarah@berkeley.edu>
2025-12-13 00:55:16 -0800

    added client list with status badges

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+clients = [
+    {"name": "Alex", "last_checkin": "2 days ago", "status": "on_track"},
+    {"name": "Jordan", "last_checkin": "5 days ago", "status": "at_risk"},
+    {"name": "Sam", "last_checkin": "Today", "status": "on_track"},
+    {"name": "Riley", "last_checkin": "8 days ago", "status": "at_risk"},
+]
 
 @app.get("/")
-def home():
-    return {"message": "hello world"}
+def home(request: Request):
+    return templates.TemplateResponse("dashboard.html", {
+        "request": request,
+        "coach_name": "Bashar",
+        "clients": clients
+    })
